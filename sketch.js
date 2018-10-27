86ea32379844b02aabf4d51e21de7fe1c09545cb
Apartflounder39 <43096149+Apartflounder39@users.noreply.github.com>
2018-10-27 16:40:43 -0400

    Removed h1

---
+++ sketch.js
@@ -0,0 +1,17 @@
+const IMAGE_WIDTH = 470;
+const IMAGE_HEIGHT = 370;
+
+
+
+function setup(){
+  createCanvas(IMAGE_WIDTH,IMAGE_HEIGHT)
+  background(200)
+  noFill();
+}
+
+function draw(){
+
+ellipse(75, 90, 60, 40);
+
+
+}
